76cb936473a860818c4d6825038c2d2e2ef2f26c
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-02-08 16:45:24 +0300

    AllReady
    so hard homework(

--- Work3/Program.cs
+++ Work3/Program.cs
@@ -5,4 +5,47 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
 
+void Generate2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
 
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Average(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum += arr[j, i];
+        }
+        double average = sum / arr.GetLength(0);
+        Console.Write($"{average}; ");
+    }
+}
+
+const int lines = 4;
+const int columns = 4;
+int[,] array = new int[lines, columns];
+
+Generate2DArray(array);
+Print2DArray(array);
+Average(array);
