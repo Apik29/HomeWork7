f2eb9efe3fd5e3d70e1d79d984267697e131e0fc
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-02-08 15:23:11 +0300

    Как на семинаре, просто double

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -4,3 +4,31 @@ m = 3, n = 4.
 1 - 3,3 8 - 9,9
 8 7,8 - 7,1 9 */
 
+double[,] Generate2DArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return result;
+}
+
+void Print2DArray(double[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] arr = Generate2DArray(3, 4);
+Print2DArray(arr);
